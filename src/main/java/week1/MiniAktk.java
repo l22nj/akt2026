eb924cd72eac1332e0b257d31ec144caa2f20933
l22nj <211811295+l22nj@users.noreply.github.com>
2026-02-17 15:12:29 +0200

    MiniAktk progressgit add .

--- src/main/java/week1/MiniAktk.java
+++ src/main/java/week1/MiniAktk.java
@@ -14,93 +14,65 @@ class Abifunktsioonid {
     }
 }
 
-class AvaldisPuu {
-    String info;
-    char muutuja;
-    int väärtus;
-    AvaldisPuu v;
-    AvaldisPuu p;
-
-    AvaldisPuu(String tehe) {
-        this.info = tehe;
-        this.muutuja = ' ';
-        this.väärtus = -1;
-    }
-
-    AvaldisPuu(char muutuja) {
-        this.info = "muutuja";
-        this.muutuja = muutuja;
-        this.väärtus = -1;
-    }
-
-    AvaldisPuu(int väärtus) {
-        this.info = "täisarv";
-        this.muutuja = ' ';
-        this.väärtus = väärtus;
-    }
-
-    int väärtustaPuu(Map<Character, Integer> muutujad) {
-        int vasak = v.väärtustaPuu(muutujad);
-        int parem = p.väärtustaPuu(muutujad);
-        try {
-            switch (info) {
-                case "täisarv":
-                    return väärtus;
-                case "muutuja":
-                    return muutujad.get(muutuja);
-                case "liida":
-                    return vasak + parem;
-                case "lahuta":
-                    return vasak - parem;
-                default:
-                    throw new IllegalArgumentException("Vigane avaldis!");
-            }
-        }
-        catch(Exception e) {
-            throw new IllegalArgumentException("Vigane avaldis!");
-        }
-    }
-}
-
 public class MiniAktk {
 
     // viimase commiti peaks parandama kui harjutuse valmis saan!
-    static AvaldisPuu looAvaldisPuu(String avaldis) {
-        Deque<String> lekseemid = new ArrayDeque<>();
+    static int väärtustaAvaldis(String avaldis, Map<String, Integer> muutujad) {
+        char[] avaldisMassiiv = avaldis.strip().toCharArray();
+        List<String> lekseemid = new ArrayList<>();
         String puhver = "";
-        char[] avaldisMassiiv = avaldis.toCharArray();
-        // Vaja arvestada ka hash-iga
-        // Targem viis arvude/muutujatega arvestamiseks?
-        for (char täht : avaldisMassiiv) {
-            if (täht == '+') {
-                lekseemid.push(puhver);
-                puhver = "";
-                lekseemid.push("liida");
-            }
-            else if (täht == '-') {
-                lekseemid.push(puhver);
-                puhver = "";
-                lekseemid.push("-");
-            } else {
-                // Siin pole vist vaja arvulisust testida, sellega tegeleb AvaldisPuu, lihtsalt
-                // kõik puhvrisse? See pole ka hea
-                if (Character.isLetter(täht) && puhver = "") {
-
-                }
+        // Hiljem vaatan, kas puhvri sees on tühikuid - siis vigane avaldis
+        for (char ch : avaldisMassiiv) {
+            switch (ch) {
+                case '+':
+                    lekseemid.add(puhver.strip());
+                    lekseemid.add("+");
+                    puhver = "";
+                    break;
+                case '-':
+                    lekseemid.add(puhver.strip());
+                    lekseemid.add("-");
+                    puhver = "";
+                    break;
+                default:
+                    puhver += ch;
+                    break;
             }
         }
+        int tulemus = 0;
+        try {
+            tulemus = Integer.parseInt(lekseemid.getFirst());
+        } catch (Exception e) {
+            tulemus = muutujad.get(lekseemid.getFirst());
+        }
+        if (lekseemid.size() == 1) {
+            return tulemus;
+        }
+        for (int i = 1; i < lekseemid.size() / 2; ++i) {
+            int parem = 0;
+            try {
+                parem = Integer.parseInt(lekseemid.get(2*i + 0));
+            } catch (Exception e) {
+                parem = muutujad.get(lekseemid.get(2*i + 0));
+            }
+            String tehe = lekseemid.get(2*i - 1);
+            if (tehe.equals("+")) {
+                tulemus += parem;
+            } else if (tehe.equals("-")) {
+                tulemus -= parem;
+            }
+        }
+        return tulemus;
     }
     static int väärtusta(String avaldis) {
         return 0;
     }
 
     static void töötleRida(String rida, Map<Character, Integer> muutujad) {
+        int räsiIndeks = rida.indexOf('#');
+        rida = rida.substring(0, räsiIndeks);
         if (rida.charAt(0) == ' ') {
-            rida = rida.strip();
-            if (rida.length() == 0) {
-                return;
-            }
-            if (rida.charAt(0) == '#') {
+            if (rida.strip().length() == 0) {
                 return;
             }
             throw new IllegalArgumentException("Lause alguses ei tohi olla tühikuid! Rida: " + rida);
@@ -115,9 +87,9 @@ public class MiniAktk {
             throw new IllegalArgumentException("Argumendiks peab olema .aktk fail");
         }
         var read = Files.readAllLines(new File(failinimi).toPath());
-        Map<Character, Integer> muutujad = new HashMap<>();
+        Map<String, Integer> muutujad = new HashMap<>();
         for (String rida : read) {
-            töötleRida(rida, muutujad);
+//            töötleRida(rida, muutujad);
         }
     }
 
@@ -125,6 +97,8 @@ public class MiniAktk {
     // Kas funktsioon töötleRida peaks saama sõnastiku viida argumendina?
 
     static void main(String[] args) throws IOException {
+        Map<String, Integer> muutujad = new HashMap<>();
+        System.out.println(väärtustaAvaldis("1-2+7+0", muutujad));
         for (String arg : args) {
             MiniAktk.töötleFail(arg);
         }
