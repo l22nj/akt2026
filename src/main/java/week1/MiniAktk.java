07cf42df2cba7958e23a0e15a026f6dba21dad12
l22nj <211811295+l22nj@users.noreply.github.com>
2026-02-17 09:47:03 +0200

    Edasiminekud MiniAktk-ga

--- src/main/java/week1/MiniAktk.java
+++ src/main/java/week1/MiniAktk.java
@@ -1,6 +1,9 @@
 package week1;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.*;
 
 class Abifunktsioonid {
     static Boolean kasLõpebSellega(String sõne, String lõpp) {
@@ -11,17 +14,110 @@ class Abifunktsioonid {
     }
 }
 
+class AvaldisPuu {
+    String info;
+    char muutuja;
+    int väärtus;
+    AvaldisPuu v;
+    AvaldisPuu p;
+
+    AvaldisPuu(String tehe) {
+        this.info = tehe;
+        this.muutuja = ' ';
+        this.väärtus = -1;
+    }
+
+    AvaldisPuu(char muutuja) {
+        this.info = "muutuja";
+        this.muutuja = muutuja;
+        this.väärtus = -1;
+    }
+
+    AvaldisPuu(int väärtus) {
+        this.info = "täisarv";
+        this.muutuja = ' ';
+        this.väärtus = väärtus;
+    }
+
+    int väärtustaPuu(Map<Character, Integer> muutujad) {
+        int vasak = v.väärtustaPuu(muutujad);
+        int parem = p.väärtustaPuu(muutujad);
+        try {
+            switch (info) {
+                case "täisarv":
+                    return väärtus;
+                case "muutuja":
+                    return muutujad.get(muutuja);
+                case "liida":
+                    return vasak + parem;
+                case "lahuta":
+                    return vasak - parem;
+                default:
+                    throw new IllegalArgumentException("Vigane avaldis!");
+            }
+        }
+        catch(Exception e) {
+            throw new IllegalArgumentException("Vigane avaldis!");
+        }
+    }
+}
+
 public class MiniAktk {
 
     // viimase commiti peaks parandama kui harjutuse valmis saan!
+    static AvaldisPuu looAvaldisPuu(String avaldis) {
+        Deque<String> lekseemid = new ArrayDeque<>();
+        String puhver = "";
+        char[] avaldisMassiiv = avaldis.toCharArray();
+        // Vaja arvestada ka hash-iga
+        // Targem viis arvude/muutujatega arvestamiseks?
+        for (char täht : avaldisMassiiv) {
+            if (täht == '+') {
+                lekseemid.push(puhver);
+                puhver = "";
+                lekseemid.push("liida");
+            }
+            else if (täht == '-') {
+                lekseemid.push(puhver);
+                puhver = "";
+                lekseemid.push("-");
+            } else {
+                // Siin pole vist vaja arvulisust testida, sellega tegeleb AvaldisPuu, lihtsalt
+                // kõik puhvrisse? See pole ka hea
+                if (Character.isLetter(täht) && puhver = "") {
 
-    static void töötleRida(String rida) {
+                }
+            }
+        }
+    }
+    static int väärtusta(String avaldis) {
+        return 0;
+    }
 
+    static void töötleRida(String rida, Map<Character, Integer> muutujad) {
+        if (rida.charAt(0) == ' ') {
+            rida = rida.strip();
+            if (rida.length() == 0) {
+                return;
+            }
+            if (rida.charAt(0) == '#') {
+                return;
+            }
+            throw new IllegalArgumentException("Lause alguses ei tohi olla tühikuid! Rida: " + rida);
+        }
+        if (rida.substring(0, 6).equals("print ")) {
+            System.out.println(väärtusta(rida.substring(6)));
+        }
     }
 
     static void töötleFail(String failinimi) throws IOException {
         if (!Abifunktsioonid.kasAktkFail(failinimi)) {
             throw new IllegalArgumentException("Argumendiks peab olema .aktk fail");
+        }
+        var read = Files.readAllLines(new File(failinimi).toPath());
+        Map<Character, Integer> muutujad = new HashMap<>();
+        for (String rida : read) {
+            töötleRida(rida, muutujad);
         }
     }
 
