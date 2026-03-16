af1baf259b39134f7d7a0d06baded085a2091656
l22nj <211811295+l22nj@users.noreply.github.com>
2026-03-16 10:28:22 +0200

    nit FiniteAutomaton

--- src/main/java/week3/FiniteAutomaton.java
+++ src/main/java/week3/FiniteAutomaton.java
@@ -43,7 +43,7 @@ public class FiniteAutomaton extends AbstractAutomaton {
 
     @Override
     public Set<Integer> getStates() {
-        return states;
+        return Set.copyOf(states);
     }
 
     @Override
@@ -53,7 +53,7 @@ public class FiniteAutomaton extends AbstractAutomaton {
 
     @Override
     public Set<Integer> getAcceptingStates() {
-        return acceptingStates;
+        return Set.copyOf(acceptingStates);
     }
 
     @Override
