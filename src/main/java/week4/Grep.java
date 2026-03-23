63dcb62df1089261040e848780a2c3322d224021
l22nj <211811295+l22nj@users.noreply.github.com>
2026-03-23 16:02:04 +0200

    tegin kodutööd grep edasi

--- src/main/java/week4/Grep.java
+++ src/main/java/week4/Grep.java
@@ -72,7 +72,143 @@ public class Grep {
      * kui teda kutsutakse välja mitu korda järjest.)
      */
     public static FiniteAutomaton regexToFiniteAutomaton(RegexNode regex) {
-        throw new UnsupportedOperationException();
+        return switch (regex) {
+            case Alternation alternation -> {
+                FiniteAutomaton v = regexToFiniteAutomaton(alternation.left());
+                FiniteAutomaton p = regexToFiniteAutomaton(alternation.right());
+                FiniteAutomaton rööbitus = rööbitaAutomaadid(v,p);
+                yield tagastaTulemus(rööbitus);
+            }
+            case Concatenation concatenation -> {
+                FiniteAutomaton v = regexToFiniteAutomaton(concatenation.left());
+                FiniteAutomaton p = regexToFiniteAutomaton(concatenation.right());
+                FiniteAutomaton summa = liidaAutomaadid(v,p);
+                yield tagastaTulemus(summa);
+            }
+            case Epsilon epsilon -> {
+                yield tagastaTulemus(null);
+            }
+            case Letter letter -> {
+                FiniteAutomaton täht = looTähtAutomaat(letter.symbol());
+                yield tagastaTulemus(täht);
+            }
+            case Repetition repetition -> {
+                FiniteAutomaton laps = regexToFiniteAutomaton(repetition.child());
+                laps.addTransition(0, null, -1);
+                laps.addTransition(-1, null, 0);
+                yield tagastaTulemus(laps);
+            }
+        };
+    }
+
+    private static FiniteAutomaton looTähtAutomaat(char symbol) {
+        FiniteAutomaton tulemus = new FiniteAutomaton();
+        tulemus.addState(0);
+        tulemus.addState(-1);
+        tulemus.setStartState(0);
+        tulemus.addAcceptingState(-1);
+        tulemus.addTransition(0, symbol, -1);
+        return tulemus;
+    }
+
+    private static FiniteAutomaton rööbitaAutomaadid(FiniteAutomaton v, FiniteAutomaton p) {
+        FiniteAutomaton tulemus = new FiniteAutomaton();
+        FiniteAutomaton uus_v = nihutaOlekuidRööbitusVasak(v, p);
+        FiniteAutomaton uus_p = nihutaOlekuidRööbitusParem(v, p);
+
+        lisaAndmed(uus_v, tulemus);
+        lisaAndmed(uus_p, tulemus);
+
+        tulemus.setStartState(0);
+        tulemus.addAcceptingState(-1);
+
+        return tulemus;
+    }
+
+    private static FiniteAutomaton liidaAutomaadid(FiniteAutomaton v, FiniteAutomaton p) {
+        FiniteAutomaton tulemus = new FiniteAutomaton();
+        FiniteAutomaton uus_v = nihutaOlekuidSummaVasak(v, p);
+        FiniteAutomaton uus_p = nihutaOlekuidSummaParem(v, p);
+
+        lisaAndmed(uus_v, tulemus);
+        lisaAndmed(uus_p, tulemus);
+
+        tulemus.setStartState(0);
+        tulemus.addAcceptingState(-1);
+
+        return tulemus;
+    }
+
+    private static void lisaAndmed(FiniteAutomaton automaat, FiniteAutomaton tulemus) {
+        for (int olek : automaat.getStates()) {
+            tulemus.addState(olek);
+            for (Character süm : automaat.getOutgoingLabels(olek)) {
+                for (int siht : automaat.getDestinations(olek, süm)) {
+                    tulemus.addState(siht);
+                    tulemus.addTransition(olek, süm, siht);
+                }
+            }
+        }
+    }
+
+    private static FiniteAutomaton nihutaOlekuidRööbitusParem(FiniteAutomaton v, FiniteAutomaton p) {
+        return null;
+    }
+
+    private static FiniteAutomaton nihutaOlekuidRööbitusVasak(FiniteAutomaton v, FiniteAutomaton p) {
+        return null;
+    }
+
+    private static FiniteAutomaton nihutaOlekuidSummaParem(FiniteAutomaton v, FiniteAutomaton p) {
+        return null;
+    }
+
+    private static FiniteAutomaton nihutaOlekuidSummaVasak(FiniteAutomaton v, FiniteAutomaton p) {
+        return null;
+    }
+
+    private static FiniteAutomaton normeeriOlekud(FiniteAutomaton automaat) {
+        FiniteAutomaton tulemus = new FiniteAutomaton();
+        int n = tulemus.getStates().size();
+
+        for (int olek : automaat.getStates()) {
+            int uus_olek = olek > -1 ? olek + 1 : n;
+            tulemus.addState(uus_olek);
+            for (Character süm : automaat.getOutgoingLabels(olek)) {
+                for (int siht : automaat.getDestinations(olek, süm)) {
+                    int uus_siht = siht > -1 ? siht + 1 : n;
+                    tulemus.addState(uus_siht);
+                    tulemus.addTransition(uus_olek, süm, uus_siht);
+                }
+            }
+        }
+
+        tulemus.setStartState(1);
+        tulemus.addAcceptingState(n);
+
+        return tulemus;
+    }
+
+    private static FiniteAutomaton tagastaTulemus(FiniteAutomaton automaat) {
+        FiniteAutomaton tulemus = new FiniteAutomaton();
+        tulemus.addState(0);
+        tulemus.addState(-1);
+        tulemus.setStartState(0);
+        tulemus.addAcceptingState(-1);
+
+        if (Objects.isNull(automaat)) {
+            tulemus.addTransition(0, null, -1);
+            return tulemus;
+        }
+
+        FiniteAutomaton uus_automaat = normeeriOlekud(automaat);
+        lisaAndmed(uus_automaat, tulemus);
+        int n = uus_automaat.getStates().size();
+
+        tulemus.addTransition(0, null, 1);
+        tulemus.addTransition(n, null, -1);
+
+        return tulemus;
     }
 
     /**
