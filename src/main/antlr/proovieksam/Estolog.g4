6e17bb43d874b7351db43648ca4d8d6c542861dd
l22nj <211811295+l22nj@users.noreply.github.com>
2026-05-18 16:20:07 +0300

    palju asju tehtud

--- src/main/antlr/proovieksam/Estolog.g4
+++ src/main/antlr/proovieksam/Estolog.g4
@@ -7,5 +7,44 @@ init : prog EOF;
 // Seda reeglit tuleb muuta / täiendada
 // (Ilmselt soovid ka defineerida uusi abireegleid)
 prog
-    : 'implementeeri mind!'
+    : (definitsioon ';')* avaldis
     ;
+
+definitsioon
+    : Muutuja ':=' avaldis
+    ;
+
+avaldis
+    : Arv       #Literaal
+    | Muutuja   #Muutuja
+    | '(' avaldis ')' #Sulud
+    | left=avaldis op=ningOp right=avaldis   #BinOp
+    | left=avaldisPealeVordust op=vordusOp right=avaldisPealeVordust #VordusOp
+//    | kuiLause #KuiLause
+    ;
+
+vordusOp: '=';
+
+avaldisPealeVordust
+    : Arv
+    | Muutuja
+    | '(' avaldis ')'
+    | left=avaldis op=ningOp right=avaldis
+    ;
+
+ningOp
+    : 'NING'
+    | 'VOI'
+    | 'JA'
+    ;
+
+// Operaatorid tuleks teha kihiliselt, läbilaskega. See on palju ohutum.
+// Võrduse saab kätte viies võrdusExpr otse edasi sulgude-expressioniks,
+// mitte mõlemale poole võrdusExpr ise.
+
+// Siin kõige kõrgema prioriteediga peaks olema kuiExpr
+
+Muutuja: [a-zA-Z]+;
+Arv: [01];
+
+WS: [ \t\r\n]+ -> skip;
--- src/main/java/proovieksam/EstologAst.java
+++ src/main/java/proovieksam/EstologAst.java
@@ -4,11 +4,13 @@ import org.antlr.v4.runtime.BailErrorStrategy;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import proovieksam.ast.EstologDef;
 import proovieksam.ast.EstologNode;
 import utils.ExceptionErrorListener;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import static proovieksam.EstologParser.*;
 import static proovieksam.ast.EstologNode.*;
@@ -43,6 +45,85 @@ public class EstologAst {
 
     // Implementeeri see meetod.
     private static EstologNode parseTreeToAst(ParseTree tree) {
-        throw new UnsupportedOperationException();
+        EstologBaseVisitor<EstologNode> visitor = new EstologBaseVisitor<>() {
+            @Override
+            public EstologNode visitInit(InitContext ctx) {
+                return visit(ctx.prog());
+            }
+
+            @Override
+            public EstologNode visitProg(ProgContext ctx) {
+                var definitsioonid = new ArrayList<EstologDef>();
+                for (var definitsioon : ctx.definitsioon()) {
+                    definitsioonid.add(visitDefinitsioon(definitsioon));
+                }
+                var avaldis = visit(ctx.avaldis());
+                return EstologNode.prog(avaldis, definitsioonid);
+            }
+
+            @Override
+            public EstologDef visitDefinitsioon(DefinitsioonContext ctx) {
+                return EstologNode.def(ctx.Muutuja().getText(), visit(ctx.avaldis()));
+            }
+
+            @Override
+            public EstologNode visitLiteraal(LiteraalContext ctx) {
+                var väärtus = ctx.Arv().getText().equals("1");
+                return EstologNode.lit(väärtus);
+            }
+
+            @Override
+            public EstologNode visitMuutuja(MuutujaContext ctx) {
+                return EstologNode.var(ctx.Muutuja().getText());
+            }
+
+            @Override
+            public EstologNode visitSulud(SuludContext ctx) {
+                return visit(ctx.avaldis());
+            }
+
+            @Override
+            public EstologNode visitBinOp(BinOpContext ctx) {
+                var v = visit(ctx.left);
+                String op = ctx.op.getText();
+                var p = visit(ctx.right);
+
+                switch (op) {
+                    case ("JA"):
+                        return EstologNode.ja(v, p);
+
+                    case ("VOI"):
+                        return EstologNode.voi(v, p);
+
+                    case ("NING"):
+                        return EstologNode.ja(v, p);
+
+                    default:
+                        throw new UnsupportedOperationException("midagi valesti läinud:///");
+                }
+            }
+
+            @Override
+            public EstologNode visitBinaryOp(BinaryOpContext ctx) {
+                return super.visitBinaryOp(ctx);
+            }
+            //            @Override
+//            public EstologNode visitInit(InitContext ctx) {
+//                return visit(ctx.prog());
+//            }
+//
+//            @Override
+//            public EstologNode visitProg(ProgContext ctx) {
+//                return EstologNode.prog(ctx.avaldis(),);
+//            }
+//
+//            @Override
+//            public EstologNode visitAvaldis(AvaldisContext ctx) {
+//                String arvText = ctx.Literaal().getText();
+//                boolean arvValue = arvText.equals("1"); // grammatika lubab ainult 1 ja 0
+//                return EstologNode.lit(arvValue);
+//            }
+        };
+        return visitor.visit(tree);
     }
 }
